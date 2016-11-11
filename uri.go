d33059f0671d67850220e678aded943a7851b529
quexer <quexer@gmail.com>
2016-11-11 14:39:00 +0800

    clean URI utilities, support pixel crop

--- uri.go
+++ uri.go
@@ -8,10 +8,12 @@ import (
 var (
 	regHash = regexp.MustCompile(`[0-9a-zA-Z]{17,}`)
 	regNum  = regexp.MustCompile(`\d{6,}`)
+	regCrop = regexp.MustCompile(`/\d{2,}x\d{2,}$`)
 )
 
 func CleanURI(s string) string {
 	s = strings.Split(s, "?")[0]
 	s = regHash.ReplaceAllString(s, ":hash")
+	s = regCrop.ReplaceAllString(s, "/:crop")
 	return regNum.ReplaceAllString(s, ":num")
 }
--- uri_test.go
+++ uri_test.go
@@ -9,7 +9,8 @@ func TestCleanURI(t *testing.T) {
 		"/api/show/101834":                                                "/api/show/:num",
 		"/api/attentionCountry/add":                                       "/api/attentionCountry/add",
 		"/api/act/57e48711e4d0f314382b45c9/news?since=0&until=0&count=10": "/api/act/:hash/news",
-		"/api/img/53f6e4219b8d723f8b00002a/138x138":                       "/api/img/:hash/138x138",
+		"/api/img/53f6e4219b8d723f8b00002a/138x138":                       "/api/img/:hash/:crop",
+		"/api/138x138/a":                                                  "/api/138x138/a",
 	}
 
 	for k, v := range data {
