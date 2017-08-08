c53a01319c017ad26883312af673177f5bd9b963
zhangbo <xiaobo1128@gmail.com>
2017-08-08 15:11:02 +0800

    add http post2

--- http_client.go
+++ http_client.go
@@ -3,6 +3,7 @@ package utee
 import (
 	"crypto/tls"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -12,6 +13,9 @@ import (
 
 const (
 	MAX_HTTP_CLIENT_CONCURRENT = 1000
+
+	Content_Type_Form = "application/x-www-form-urlencoded"
+	Content_Type_Json = "application/json; charset=utf-8"
 )
 
 var (
@@ -21,6 +25,70 @@ var (
 		Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
 	}
 )
+
+type BasicAuth struct {
+	Username string
+	Password string
+}
+
+type HttpOpt struct {
+	Headers   map[string]string
+	BasicAuth *BasicAuth
+}
+
+func HttpPost2(postUrl string, contentType string, body io.Reader, opt *HttpOpt) ([]byte, error) {
+	contentType = strings.TrimSpace(contentType)
+	if contentType == "" {
+		contentType = Content_Type_Form
+	}
+
+	httpClientThrottle.Acquire()
+	defer httpClientThrottle.Release()
+
+	var resp *http.Response
+	var err error
+	req, err := http.NewRequest("POST", postUrl, body)
+	if err != nil {
+		return nil, fmt.Errorf("[http] err %s, %s", postUrl, err)
+	}
+	req.Header.Set("Content-Type", contentType)
+
+	if opt != nil {
+		for k, v := range opt.Headers {
+			k := strings.TrimSpace(k)
+			if k == "" {
+				continue
+			}
+			if strings.ToLower(k) == "content-type" {
+				continue
+			}
+
+			req.Header.Set(strings.Title(k), v)
+		}
+
+		if opt.BasicAuth != nil {
+			req.SetBasicAuth(opt.BasicAuth.Username, opt.BasicAuth.Password)
+		}
+	}
+
+	resp, err = insecureClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("[http] err %s, %s", postUrl, err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("[http] status err %s, %d", postUrl, resp.StatusCode)
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("[http] read err %s, %s", postUrl, err)
+	}
+
+	return b, nil
+}
 
 func HttpPost(postUrl string, q url.Values, credential ...string) ([]byte, error) {
 	httpClientThrottle.Acquire()
