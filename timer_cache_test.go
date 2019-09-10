40784d9e76370b65675bf0392b1d255ad937358b
quexer <quexer@gmail.com>
2019-09-10 14:51:35 +0800

    timer cache: expire test

--- timer_cache_test.go
+++ timer_cache_test.go
@@ -8,8 +8,7 @@ import (
 var _ = Describe("TimerCache", func() {
 	var tc *TimerCache
 	BeforeEach(func() {
-		tc = NewTimerCache(1, func(k, v interface{}) {
-		})
+		tc = NewTimerCache(1, nil)
 	})
 	It("auto clear after expire", func() {
 		By("brand new time cache should be empty", func() {
@@ -33,5 +32,16 @@ var _ = Describe("TimerCache", func() {
 		tc.Put(1, "a")
 		tc.Remove(1)
 		Expect(tc.Len()).Should(Equal(0))
+	})
+	It("expire", func() {
+		var key int
+		var val string
+		tc = NewTimerCache(1, func(k, v interface{}){
+			key = k.(int)
+			val = v.(string)
+		})
+		tc.Put(1, "a")
+		Eventually(func() int { return key }, 2).Should(Equal(1))
+		Eventually(func() string { return val }, 2).Should(Equal("a"))
 	})
 })
